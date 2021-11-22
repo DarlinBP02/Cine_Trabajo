91c0557721b3f0e674e4238b297bdb3d59c521dd
DARLIN ALEXANDER BRAVO PONCE <darlinbravo02@outlook.com>
2021-11-22 18:34:31 -0500

    Agregar archivos de proyecto.

---
+++ Cine_Trabajo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cine_Trabajo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
