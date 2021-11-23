2d68e16011c3c2c5a405fb05b71bd2cebf190dde
myckellsamaniego <mikesamaniego99@gmail.com>
2021-11-22 19:56:28 -0500

    agregando la clase sala con su respectivo metodo

--- Cine_Trabajo/Asiento.cs
+++ Cine_Trabajo/Asiento.cs
@@ -6,5 +6,43 @@ namespace Cine_Trabajo
 {
     class Asiento
     {
+        //CONSTRUCTOR
+        public Asiento()
+        {
+
+        }
+        //Atributos
+        int fila, columnas;
+        int[,] asiento = new int[20, 40];
+
+        //METODO PARA ELEGIR ASIENTO
+        public void asientos()
+        {
+
+            Console.WriteLine("************");
+            Console.WriteLine("*     Seleccione el asiento    *");
+            Console.WriteLine("************");
+            Console.WriteLine("Ingrese el numero de fila (1-20)");
+            fila = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero de columnas (1-40)");
+            columnas = int.Parse(Console.ReadLine());
+            if (asiento[fila - 1, columnas - 1] == 0)
+            {
+                asiento[fila - 1, columnas - 1] = 1;
+                Console.WriteLine("Asiento seleccionado");
+            }
+            else
+                Console.WriteLine("Asiento ocupado");
+        }
+        //METODO PARA MOSTRAR ASIENTO SELECCIONADO
+        public void mostrarasiento()
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Con el asiento seleccionado");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Columna => " + columnas);
+            Console.WriteLine("Fila => " + fila);
+        }
+
     }
 }
--- Cine_Trabajo/Sala.cs
+++ Cine_Trabajo/Sala.cs
@@ -6,6 +6,7 @@ namespace Cine_Trabajo
 {
     class Sala
     {
-       
+
     }
 }
+
